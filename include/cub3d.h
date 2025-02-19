752d4fa6ef13f8067d93d8b96eddcc3969d21b33
mfleury <mfleury@student.42barcelona.com>
2025-02-19 14:53:24 +0100

    created cub3d.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,13 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.c                                            :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/19 14:47:27 by mfleury           #+#    #+#             */
-/*   Updated: 2025/02/19 14:47:46 by mfleury          ###   ########.fr       */
+/*   Created: 2025/02/19 14:47:58 by mfleury           #+#    #+#             */
+/*   Updated: 2025/02/19 14:52:41 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <stdio.h>
+
+# include "../libft/libft.h"
+# include "../minilibx/mlx.h"
+
+#endif
