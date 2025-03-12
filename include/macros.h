7df7cae75070333fd2a7bd228727b42d0064f70a
mpietrza <mpietrza@student.42barcelona.com>
2025-03-12 12:51:57 +0100

    merge with main done

--- include/macros.h
+++ include/macros.h
@@ -6,7 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 16:57:57 by mpietrza          #+#    #+#             */
-/*   Updated: 2025/03/03 17:03:43 by mpietrza         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:50:41 by mpietrza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 # define MACROS_H
 
 //boolean values for int
-#define TRUE	1
-#define FALSE	0
+# define TRUE	1
+# define FALSE	0
 
 //colors
-#define RST	"\033[0m"
-#define BLK	"\033[30m"
-#define RED	"\033[31m"  
-#define GRN	"\033[32m"
-#define YLW	"\033[33m"
-#define BLU	"\033[34m"
-#define MGN	"\033[35m"
-#define CYN	"\033[36m"
-#define WHT	"\033[37m"
-#define BLD	"\033[1m" 
+# define RST	"\033[0m"
+# define BLK	"\033[30m"
+# define RED	"\033[31m"  
+# define GRN	"\033[32m"
+# define YLW	"\033[33m"
+# define BLU	"\033[34m"
+# define MGN	"\033[35m"
+# define CYN	"\033[36m"
+# define WHT	"\033[37m"
+# define BLD	"\033[1m" 
 
 #endif
