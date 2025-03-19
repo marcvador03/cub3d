5aa6aa263edc11932f502ccf432f466d9b217e41
Marc Fleury <mfleury03@gmail.com>
2025-03-19 17:21:54 +0100

    merge all structure declarations in data.h

--- include/data.h
+++ include/data.h
@@ -6,7 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:50:03 by mpietrza          #+#    #+#             */
-/*   Updated: 2025/03/12 12:50:08 by mpietrza         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:17:59 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,64 @@
 # define DATA_H
 
 # include "cub3d.h"
+
+typedef struct	s_render
+{
+	double	wall_x;
+	int		pixel_x;
+	int		pixel_y;
+	double	step;
+	double	pixel_pos;
+
+}	t_render;
+
+typedef struct s_player
+{
+	double	pos_x;
+	double 	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	camera_x;
+
+}	t_player;
+
+typedef struct s_raycast
+{
+	double	raydir_x;
+	double	raydir_y;
+	double	deltadist_x;
+	double	deltadist_y;
+	int		step_x;
+	int		step_y;
+	double	sidedist_x;
+	double	sidedist_y;
+	int		map_x;
+	int		map_y;
+	int		hit_flag;
+	int		side_flag;
+	double	walldist;
+	int		lineheight;
+	int		wall_start;
+	int		wall_end;
+
+}	t_raycast;
+
+typedef struct s_mlx
+{
+	mlx_t			*mlx;
+	t_raycast		*raycast;
+	t_player		*player;
+	t_render		*render;
+	char			*map_path;
+	int				**map;
+	int				win_w;
+	int				win_h;
+	mlx_image_t		*image;
+	mlx_texture_t	*texture;
+}	t_mlx;
+
 
 typedef struct s_gr_d
 {
