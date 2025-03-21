835c754630183bd93eeaf4247e002dbb8e390427
Marc Fleury <mfleury03@gmail.com>
2025-03-21 14:06:56 +0100

    moved back struct to cub3d.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 14:47:58 by mfleury           #+#    #+#             */
-/*   Updated: 2025/03/21 12:58:40 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/03/21 14:04:32 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,104 @@
 
 # define FALSE 0
 # define TRUE 1
+
+/* !!! struct for storing all the data !!! */
+typedef struct	s_render
+{
+	double	wall_x;
+	int		pixel_x;
+	int		pixel_y;
+	double	step;
+	double	pixel_pos;
+
+}	t_render;
+
+typedef struct s_player
+{
+	double	pos_x;
+	double 	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	camera_x;
+
+}	t_player;
+
+typedef struct s_raycast
+{
+	double	raydir_x;
+	double	raydir_y;
+	double	deltadist_x;
+	double	deltadist_y;
+	int		step_x;
+	int		step_y;
+	double	sidedist_x;
+	double	sidedist_y;
+	int		map_x;
+	int		map_y;
+	int		hit_flag;
+	int		side_flag;
+	double	walldist;
+	int		lineheight;
+	int		wall_start;
+	int		wall_end;
+
+}	t_raycast;
+
+typedef struct s_gr_d
+{
+	char			*no_tx;
+	char			*so_tx;
+	char			*we_tx;
+	char			*ea_tx;
+	unsigned int	f_clr[3];
+	unsigned int	c_clr[3];
+}	t_gr_d;
+
+/* struct for storing position on a map*/
+typedef struct s_pos
+{
+	int			x;
+	int			y;
+}				t_pos;
+
+/* struct for creating and managing the map */
+typedef struct s_map
+{
+	char	**arr;
+	t_pos	*map_size;
+	t_pos	*pl_pos;
+	char	player_dir;
+	int		x;
+	int		y;
+}		t_map;
+
+typedef struct s_data
+{
+	t_gr_d		*txs;
+	t_map		*map;
+	t_list		*ln_lst;
+	t_list		*map_lst;
+	//t_mlx		*cub;
+}	t_data;
+
+typedef struct s_mlx
+{
+	mlx_t			*mlx;
+	t_raycast		*raycast;
+	t_player		*player;
+	t_render		*render;
+	char			*map_path;
+	int				**map;
+	int32_t			win_w;
+	int32_t			win_h;
+	mlx_image_t		*image;
+	mlx_texture_t	*texture;
+	t_data			*d;
+}	t_mlx;
+
+
 
 
 /* --------------------- utils functions -------------------------------------*/
--- include/data.h
+++ include/data.h
@@ -6,7 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:50:03 by mpietrza          #+#    #+#             */
-/*   Updated: 2025/03/19 17:17:59 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/03/21 13:44:49 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,100 +15,5 @@
 
 # include "cub3d.h"
 
-typedef struct	s_render
-{
-	double	wall_x;
-	int		pixel_x;
-	int		pixel_y;
-	double	step;
-	double	pixel_pos;
-
-}	t_render;
-
-typedef struct s_player
-{
-	double	pos_x;
-	double 	pos_y;
-	double	dir_x;
-	double	dir_y;
-	double	plane_x;
-	double	plane_y;
-	double	camera_x;
-
-}	t_player;
-
-typedef struct s_raycast
-{
-	double	raydir_x;
-	double	raydir_y;
-	double	deltadist_x;
-	double	deltadist_y;
-	int		step_x;
-	int		step_y;
-	double	sidedist_x;
-	double	sidedist_y;
-	int		map_x;
-	int		map_y;
-	int		hit_flag;
-	int		side_flag;
-	double	walldist;
-	int		lineheight;
-	int		wall_start;
-	int		wall_end;
-
-}	t_raycast;
-
-typedef struct s_mlx
-{
-	mlx_t			*mlx;
-	t_raycast		*raycast;
-	t_player		*player;
-	t_render		*render;
-	char			*map_path;
-	int				**map;
-	int				win_w;
-	int				win_h;
-	mlx_image_t		*image;
-	mlx_texture_t	*texture;
-}	t_mlx;
-
-
-typedef struct s_gr_d
-{
-	char			*no_tx;
-	char			*so_tx;
-	char			*we_tx;
-	char			*ea_tx;
-	unsigned int	f_clr[3];
-	unsigned int	c_clr[3];
-}	t_gr_d;
-
-/* struct for storing position on a map*/
-typedef struct s_pos
-{
-	int			x;
-	int			y;
-}				t_pos;
-
-/* struct for creating and managing the map */
-typedef struct s_map
-{
-	char	**arr;
-	t_pos	*map_size;
-	t_pos	*pl_pos;
-	char	player_dir;
-	int		x;
-	int		y;
-}		t_map;
-
-/* !!! struct for storing all the data !!! */
-typedef struct s_data
-{
-	t_gr_d		*txs;
-	t_map		*map;
-	t_list		*ln_lst;
-	t_list		*map_lst;
-	//t_mlx		*cub;
-}	t_data;
 
 #endif
