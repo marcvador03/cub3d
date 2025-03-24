0ec16a091a914328e7607329e21b91ea49815751
Marc Fleury <mfleury03@gmail.com>
2025-03-24 12:10:39 +0100

    inserted window height and width var

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,11 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 14:47:27 by mfleury           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/03/22 17:28:36 by mpietrza         ###   ########.fr       */
-=======
-/*   Updated: 2025/03/21 16:57:09 by mfleury          ###   ########.fr       */
->>>>>>> 2279608834c29539da66f9740ef6b29b0ed880ce
+/*   Updated: 2025/03/24 12:08:56 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +23,11 @@ int	main(int argc, char **argv)
 		cub.map_path = argv[1];
 	d = (t_data *)safe_malloc(sizeof(t_data), NULL);
 	parsing_process(&cub, d);
-<<<<<<< HEAD
-	cub.mlx = mlx_init(1920, 1080, "CUB3D", true);
-=======
 	//exit(0);
 	cub.d = d;
 	cub.win_w = 1080;
 	cub.win_h = 1080;
 	cub.mlx = mlx_init(cub.win_w, cub.win_h, "CUB3D", false);
->>>>>>> 2279608834c29539da66f9740ef6b29b0ed880ce
 	if (cub.mlx == NULL)
 		return (1);
 	mlx_close_hook(cub.mlx, &hook_close, &cub);
@@ -43,9 +35,6 @@ int	main(int argc, char **argv)
 	raycast_init(&cub);
 	mlx_loop(cub.mlx);
 	mlx_terminate(cub.mlx);
-<<<<<<< HEAD
-=======
 	free_data(d);
->>>>>>> 2279608834c29539da66f9740ef6b29b0ed880ce
 	return (0);
 }
