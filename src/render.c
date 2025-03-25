d92f22eecf4d85cda478bf811808a0fe075a3119
mfleury <mfleury@student.42barcelona.com>
2025-03-25 16:50:29 +0100

    norminette

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 12:45:43 by mfleury           #+#    #+#             */
-/*   Updated: 2025/03/25 16:43:58 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/03/25 16:49:58 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 static uint32_t	get_rgba(mlx_texture_t	*t, int index)
 {
-    uint32_t color;
+	uint32_t	color;
 
-    if (index < 0 || index >= (int)(t->width * t->height))
-    {
-        fprintf(stderr, "Error: index out of bounds: %d\n", index);
-        return 0; // Default color
-    }
-    color = (t->pixels[index * BPP] << 24);
-    color |= (t->pixels[index * BPP + 1] << 16);
-    color |= (t->pixels[index * BPP + 2] << 8);
-    color |= (t->pixels[index * BPP + 3]);
-    return color;
+	if (index < 0 || index >= (int)(t->width * t->height))
+	{
+		fprintf(stderr, "Error: index out of bounds: %d\n", index);
+		return (0); // Default color
+	}
+	color = (t->pixels[index * BPP] << 24);
+	color |= (t->pixels[index * BPP + 1] << 16);
+	color |= (t->pixels[index * BPP + 2] << 8);
+	color |= (t->pixels[index * BPP + 3]);
+	return (color);
 }
 
 static void	render_loop(t_data *d, t_render *r, int x)
@@ -33,7 +33,6 @@ static void	render_loop(t_data *d, t_render *r, int x)
 	int			y;
 	uint32_t	color;
 	uint32_t	height;
-	
 
 	y = 0;
 	while (y < d->win_h)
@@ -41,7 +40,7 @@ static void	render_loop(t_data *d, t_render *r, int x)
 		if (y >= d->raycast->wall_start && y <= d->raycast->wall_end)
 		{
 			height = d->texture->height;
-			r->pixel_y = (int) r->pixel_pos & (height- 1);
+			r->pixel_y = (int) r->pixel_pos & (height - 1);
 			r->pixel_pos += r->step;
 			color = get_rgba(d->texture, (height * r->pixel_y + r->pixel_x));
 			mlx_put_pixel(d->image, d->win_w - x - 1, y++, color);
